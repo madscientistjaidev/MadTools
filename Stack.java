4098312c30f34e4f997e41bfdb73b0d152cb5f2c
Mad Scientist <madscientistjaidev@gmail.com>
2017-07-01 23:52:29 -0400

    Docs added to Stack.

--- Stack.java
+++ Stack.java
@@ -14,33 +14,75 @@ import java.util.Scanner;
 public class Stack<T> implements Iterable
 {
 
-    // ArrayList stores Stack elements
+    // Stores Stack elements
     private ArrayList<T> a = new ArrayList<>();
 
     // Maximum size of Stack. 0 for unlimited.
     private int m = 0;
 
-    // Constructors to initialize Stack from various data structures
+    /**
+	 * Constructor to initialize Stack.
+	 */
     Stack() {}
-    Stack(ArrayList<T> a) {this.a = a;}
-    Stack(Collection<T> a) {this.a = new ArrayList(a);}
-    Stack(List<T> a) {this.a = new ArrayList(a);}
-    Stack(Stack<T> a) {this.a = reverse(a.popToArrayList());}
-    Stack(T[] a) {this.a = new ArrayList(Arrays.asList(a));}
+    
+	/**
+	 * Constructor to initialize Stack.
+	 * @param a 
+	 */
+	Stack(ArrayList<T> a) {this.a = a;}
+    
+	/**
+	 * Constructor to initialize Stack.
+	 * @param a 
+	 */
+	Stack(Collection<T> a) {this.a = new ArrayList(a);}
+    
+	/**
+	 * Constructor to initialize Stack.
+	 * @param a 
+	 */
+	Stack(List<T> a) {this.a = new ArrayList(a);}
+    
+	/**
+	 * Constructor to initialize Stack.
+	 * @param a 
+	 */
+	Stack(Stack<T> a) {this.a = reverse(a.popToArrayList());}
+    
+	/**
+	 * Constructor to initialize Stack.
+	 * @param a 
+	 */
+	Stack(T[] a) {this.a = new ArrayList(Arrays.asList(a));}
 
-    // Pops and returns element at Stack top.
+    /**
+	 * Pops and returns element at Stack top.
+	 * @return 
+	 */
     T pop() {return a.isEmpty() ? null : a.remove(a.size() - 1);}
 
-    // Views but does not pop element at Stack top.
+    /**
+	 * Views but does not pop element at Stack top.
+	 * @return 
+	 */
     T peek() {return a.get(a.size() - 1);}
 
-    // Removes all elements from stack
+    /**
+	 * Removes all elements from stack.
+	 */
     void clear() {a.clear();}
 
-    // Compares two Stacks
+    /**
+	 * Compares two Stacks.
+	 * @param x
+	 * @return 
+	 */
     boolean equals(Stack<T> x) {return x.hash() == a.hashCode();}
 
-    // Pops elements serially. Allows Stack to be target of Enhanced For Loop
+    /**
+	 * Pops elements serially. Allows Stack to be target of Enhanced For Loop.
+	 * @return 
+	 */
     @Override
     public Iterator iterator() {
         ArrayList<T> x = reverse(a);
@@ -50,7 +92,10 @@ public class Stack<T> implements Iterable
         return x.iterator();
     }
 
-    // Pops Stack elements into an array
+    /**
+	 * Pops Stack elements into an array.
+	 * @return 
+	 */
     T[] popToArray()
     {
         T[] x = (T[]) (reverse(a)).toArray();
@@ -59,7 +104,10 @@ public class Stack<T> implements Iterable
         return x;
     }
 
-    // Pops Stack elements into an ArrayList
+    /**
+	 * Pops Stack elements into an ArrayList.
+	 * @return 
+	 */
     ArrayList<T> popToArrayList()
     {
         ArrayList<T> x = reverse(a);
@@ -68,25 +116,46 @@ public class Stack<T> implements Iterable
         return x;
     }
 
-    // Checks if Stack is empty
+    /**
+	 * Checks if Stack is empty.
+	 * @return 
+	 */
     boolean isEmpty() {return a.isEmpty();}
 
-    // Checks is Stack is full
+    /**
+	 * Checks is Stack is full.
+	 * @return 
+	 */
     boolean hasSpace() {return (m == 0) ? true : a.size() < m;}
 
-    // Returns Hash
+    /**
+	 * Returns Hash code.
+	 * @return 
+	 */
     int hash() {return a.hashCode();}
 
-    // Returns size
+    /**
+	 * Returns size.
+	 * @return 
+	 */
     int size() {return a.size();}
 
-    // Returns maximum size. 0 for unlimited.
+    /**
+	 * Returns maximum size. 0 for unlimited.
+	 * @return 
+	 */
     int getMax() {return m;}
 
-    // Removes size limit
+    /**
+	 * Removes size limit.
+	 */
     void clearMax() {m = 0;}
 
-    // Sets size limit
+    /**
+	 * Sets size limit.
+	 * @param m
+	 * @return 
+	 */
     boolean setMax(int m)
     {
         if (a.size() > m) return false;
@@ -95,7 +164,11 @@ public class Stack<T> implements Iterable
         return true;
     }
 
-    // Sets maximum size and discards all overflowing elements
+    /**
+	 * Sets maximum size and discards all overflowing elements.
+	 * @param m
+	 * @return 
+	 */
     int truncate(int m)
     {
         this.m = m;
@@ -105,13 +178,46 @@ public class Stack<T> implements Iterable
         return a.size() - m;
     }
 
-    // Pushes various data structures onto Stack
+    /**
+	 * Pushes onto Stack.
+	 * @param s
+	 * @return 
+	 */
     boolean push(T s) {return (hasSpace()) ? false : a.add(s);}
-    boolean push(T s[]) {return (hasSpace() && (a.size() + s.length) >= m) ? false : a.addAll(Arrays.asList(s));}
-    boolean push(ArrayList<T> s) {return (hasSpace() && (a.size() + s.size()) >= m) ? false : a.addAll(s);}
-    boolean push(List<T> s) {return (hasSpace() && (a.size() + s.size()) >= m) ? false : a.addAll(s);}
-    boolean push(Collection<T> s) {return (hasSpace() && (a.size() + s.size()) >= m) ? false : a.addAll(s);}
+    
+	/**
+	 * Pushes onto Stack.
+	 * @param s
+	 * @return 
+	 */
+	boolean push(T s[]) {return (hasSpace() && (a.size() + s.length) >= m) ? false : a.addAll(Arrays.asList(s));}
+    
+	/**
+	 * Pushes onto Stack.
+	 * @param s
+	 * @return 
+	 */
+	boolean push(ArrayList<T> s) {return (hasSpace() && (a.size() + s.size()) >= m) ? false : a.addAll(s);}
+    
+	/**
+	 * Pushes onto Stack.
+	 * @param s
+	 * @return 
+	 */
+	boolean push(List<T> s) {return (hasSpace() && (a.size() + s.size()) >= m) ? false : a.addAll(s);}
+    
+	/**
+	 * Pushes onto Stack.
+	 * @param s
+	 * @return 
+	 */
+	boolean push(Collection<T> s) {return (hasSpace() && (a.size() + s.size()) >= m) ? false : a.addAll(s);}
 
+	/**
+	 * Pushes onto Stack.
+	 * @param s
+	 * @return 
+	 */
     boolean push(Stack<T> s)
     {
         if (hasSpace() && (a.size() + s.size()) >= m) return false;
@@ -120,6 +226,12 @@ public class Stack<T> implements Iterable
         return true;
     }
 
+	/**
+	 * Pushes onto Stack.
+	 * @param f
+	 * @return
+	 * @throws FileNotFoundException 
+	 */
     boolean push(File f) throws FileNotFoundException
     {
         Scanner           sc = new Scanner(f);
@@ -128,11 +240,15 @@ public class Stack<T> implements Iterable
         while (sc.hasNext()) l.add(sc.nextLine());
         if ((hasSpace() && (a.size() + l.size()) >= m)) return false;
         
-        for (String s : l) a.add((T) s);
+		l.forEach((s) -> {a.add((T) s);});
         return true;
     }
 
-    // Fits elements into Stack and returns number of elements pushed
+    /**
+	 * Fits elements into Stack and returns number of elements pushed.
+	 * @param s
+	 * @return 
+	 */
     int fit(T s[])
     {
         int c = 0;
@@ -141,6 +257,12 @@ public class Stack<T> implements Iterable
         return c;
     }
 
+	/**
+	 * Fits elements into Stack and returns number of elements pushed.
+	 * @param f
+	 * @return
+	 * @throws FileNotFoundException 
+	 */
     int fit(File f) throws FileNotFoundException
     {
         Scanner           sc = new Scanner(f);
@@ -161,41 +283,60 @@ public class Stack<T> implements Iterable
         return c;
     }
 
-    // Used inernally for Stack operations
+    /**
+	 * Used internally for Stack operations.
+	 * @param x
+	 * @return 
+	 */
     private ArrayList<T> reverse(ArrayList<T> x)
     {
         ArrayList<T> t = new ArrayList<>();
 
-        for (T l : x)t.add(x.remove(x.size() - 1));
+		x.forEach((_item) -> {t.add(x.remove(x.size()-1));});
         return t;
     }
 
-    // Inverts Stack
+    /**
+	 * Inverts Stack.
+	 * @return 
+	 */
     Stack<T> invert()
     {
         Stack<T> s = new Stack<>();
 
-        for (T l : a)s.push(l);
+		a.forEach((l) -> {s.push(l);});
+		
         return s;
     }
 
-    // Writes elements of Stack to a file
-    File toFile(String p) throws IOException
+    /**
+	 * Writes elements of Stack to a file.
+	 * @param p
+	 * @param delimiter
+	 * @return
+	 * @throws IOException 
+	 */
+    File toFile(String p, String delimiter) throws IOException
     {
         File f = new File(p);
         if (f.createNewFile() == false) throw new IOException("Unable to create file");
         
         PrintStream s = new PrintStream(f);
-        for (T x : reverse(a)) s.println(x.toString());
+		reverse(a).forEach((x) -> {s.print(x.toString()+delimiter);});
 
         return f;
     }
 
-    // Returns Integer Stack with Hashcode of each element
+    /**
+	 * Returns Integer Stack with Hash code of each element.
+	 * @return 
+	 */
     Stack<Integer> hashStack()
     {
         Stack<Integer> s = new Stack<>();
-        for (T t : reverse(a)) s.push(t.hashCode());
+		
+		reverse(a).forEach((t) -> {	s.push(t.hashCode());});
+		
         return s;
     }
 }
