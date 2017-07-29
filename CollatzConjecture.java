a57bf3caf9d0c8d5d8973bbebce5e01c3b288b99
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2017-07-29 10:20:21 -0400

    Slight optimizations

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -15,21 +15,26 @@ public class CollatzConjecture
 		
 		if(n<1) return result;
 		
-		do result.add(n = n%2==0 ? n /= 2 : 3*n+1);
-		while(n!=1);
+		result.add(n);
+		
+		do
+			result.add(result.get(result.size()-1)%2==0 ?
+				result.get(result.size()-1)/2 :
+				3*(result.get(result.size()-1))+1);
+		while(result.get(result.size()-1)!=1);
 		
 		return result;
 	}
 	
 	/**
 	 * Example.
-	 * Generates 25 random numbers between 0 and 2^22.
+	 * Generates 25 random numbers between 0 and 2^20.
 	 * Prints hailstorm sequence and its length for each of them.
 	 * @param args 
 	 */
 	public static void main(String args[])
 	{
-		int maxVal = (int)Math.pow(2, 22);
+		int maxVal = (int)Math.pow(2, 20);
 		int size = 25;
 		int val;
 		
@@ -38,18 +43,10 @@ public class CollatzConjecture
 		
 		for(int i = 0; i < size; i++)
 		{
-			val = (int) (maxVal*Math.random());
-			
-			System.out.print(val + " : ");
-			
+			val = (int)(maxVal*Math.random());
 			result = c.findSequence(val);
 			
-			System.out.print(result.size() + " -> ");
-			
-			for(Integer x : result)
-				System.out.print(x + ", ");
-			
-			System.out.println();
+			System.out.println(val + " : " + result.size() + " -> " + result);
 		}
 	}
 }
