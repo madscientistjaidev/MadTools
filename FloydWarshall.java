f306d5c47a8ff492f93c5ccdd7ef2d0205133f67
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2017-07-30 21:07:08 -0400

    Cleanup

--- FloydWarshall.java
+++ FloydWarshall.java
@@ -1,25 +1,27 @@
 package MadTools;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Mad Scientist
  */
 public class FloydWarshall
 {
-    int[][] run(int [][] graph)
+	int[][] run(int [][] graph)
     {
         for(int k = 0; k < graph.length; k++)
-            for (int[] graph1 : graph)
+            for (int[] row : graph)
                 for (int j = 0; j < graph.length; j++)
-                    if (graph1[j] > graph1[k] + graph[k][j])
-                        graph1[j] = graph1[k] + graph[k][j];
+                    if (row[j] > row[k] + graph[k][j])
+                        row[j] = row[k] + graph[k][j];
         
         return graph;
     }
-    
+	
     public static void main(String args[])
     {
-        int graph[][] = {
+		int graph[][] = {
                             {0,1000,-2,1000},
                             {4,0,3,1000},
                             {1000,1000,0,2},
@@ -28,10 +30,6 @@ public class FloydWarshall
         
         int [][] dist = new FloydWarshall().run(graph);
         
-        for(int i = 0; i < graph.length; i++)
-        {
-            for(int j = 0; j < graph.length; j++) System.out.print(dist[i][j] + ",");
-            System.out.println();
-        }
+        for(int[] row : dist) System.out.println(Arrays.toString(row));
     }
 }
