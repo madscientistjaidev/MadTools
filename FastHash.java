6b04100bf49d838f255d09916d079a383f3b65ac
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2017-10-25 02:27:31 -0400

    Added Hex capability

--- FastHash.java
+++ FastHash.java
@@ -45,4 +45,53 @@ public class FastHash
 	
 	return c;
     }
+	
+    public String hexHash(String s, int l)
+    {
+	return toHex(binHash(s,l));
+    }
+    
+    String toHex(String s)
+    {
+	String h = "";
+	
+	while(s.length()%4!=0) s = "0" + s;
+	
+	char n[] = s.toCharArray();
+	
+	for(int i = n.length-1; i>=0; i-=4)
+	    h=h.concat(hexDigitLookup("" + n[i-3] + n[i-2] + n[i-1] + n[i]));
+	
+	return h;
+    }
+    
+    private String hexDigitLookup(String d)
+    {
+	if(d.length()!=4) return "";
+	
+	String [][] table = {
+				{"0000","0"},
+				{"0001","1"},
+				{"0010","2"},
+				{"0011","3"},
+				{"0100","4"},
+				{"0101","5"},
+				{"0110","6"},
+				{"0111","7"},
+				{"1000","8"},
+				{"1001","9"},
+				{"1010","A"},
+				{"1011","B"},
+				{"1100","C"},
+				{"1101","D"},
+				{"1110","E"},
+				{"1111","F"},
+			    };
+	
+	for(String [] pair : table)
+	    if(pair[0].equals(d))
+		return pair[1];
+	
+	return "";
+    }
 }
