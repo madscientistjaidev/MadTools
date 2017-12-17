f9b497f37329fa62595297f89c43953460ff2f6d
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2017-12-17 01:03:03 -0500

    Streamlined code.

--- QuickSort.java
+++ QuickSort.java
@@ -9,101 +9,92 @@ import java.util.ArrayList;
 abstract class QuickSort <T>
 {        
     /**
-	 * Accepts an ArrayList and returns one sorted in ascending order.
-	 * @param a
-	 * @return 
-	 */
-	ArrayList <T> ascending(ArrayList <T> a)
+     * Accepts an ArrayList and returns one sorted in ascending order.
+     *
+     * @param a
+     * @return
+     */
+    ArrayList <T> ascending(ArrayList <T> a)
     {        
-        if(a==null) return new ArrayList<>();
-		if(a.size()<=1) return a;
+	if(a.size()<=1) return a;
         
-        //Randomly choose pivot.
-		T pivot = a.remove((int)(a.size()*Math.random()));
-        
-        ArrayList <T> left = new ArrayList<>();
-        ArrayList <T> right = new ArrayList<>();
-		
-		for(T x : a)
-            if(!compareTo(x, pivot)) left.add(x);
-            else right.add(x);
+        T pivot = a.remove((int)(a.size()*Math.random()));
+        ArrayList <T> left = new ArrayList<>(), right = new ArrayList<>();
+	
+	for (T x : a)
+	    if(!compareTo(x, pivot)) left.add(x);
+	    else right.add(x);
         
         left = ascending(left);
-        right = ascending(right);
-        
-        left.add(pivot);
-        right.forEach(left::add);
-        
-        return left;
-    }
-    
-	/**
-	 * Accepts an ArrayList and returns one sorted in descending order.
-	 * @param a
-	 * @return 
-	 */
-    ArrayList <T> descending(ArrayList <T> a)
-    {   
-       if(a==null) return new ArrayList<>();
-		if(a.size()<=1) return a;
-        
-        //Randomly choose pivot.
-		T pivot = a.remove((int)(a.size()*Math.random()));
-        
-        ArrayList <T> left = new ArrayList<>();
-        ArrayList <T> right = new ArrayList<>();
-        
-        for(T x : a)
-            if(!compareTo(x, pivot)) right.add(x);
-            else left.add(x);
-        
-        left = descending(left);
-        right = descending(right);
-        
-        left.add(pivot);
-        right.forEach(left::add);
+	left.add(pivot);
+        left.addAll(ascending(right));
         
         return left;
     }
     
     /**
-	 * This function returns true if a>b.
-	 * It is meant to make sorting possible no matter what types or objects are used.
-	 * It should be implemented in a sub class or anonymous inner class.
+     * Accepts an ArrayList and returns one sorted in descending order.
+     *
+     * @param a
+     * @return
+     */
+    ArrayList <T> descending(ArrayList <T> a)
+    {        
+        if(a.size()<=1) return a;
+        
+        T pivot = a.remove((int)(a.size()*Math.random()));
+        ArrayList <T> left = new ArrayList<>(), right = new ArrayList<>();
+	
+	for (T x : a)
+	    if(!compareTo(x, pivot)) right.add(x);
+	    else left.add(x);
+        
+        left = ascending(left);
+	left.add(pivot);
+        left.addAll(ascending(right));
+        
+        return left;
+    }
+    
+    /**
+     * This function returns true if a>b. It is meant to make sorting possible no matter what types or objects are used.
+     * It should be implemented in a sub class or anonymous inner class.
+     *
      * @param a
      * @param b
-     * @return 
+     * @return
      */
     abstract boolean compareTo(T a, T b);
-	
-	/**
-	 * Example for ~137 million randomly generated integers of size 0 to Integer.MAX_VALUE.
-	 * @param args 
-	 */
-	public static void main(String args[])
-    {		
-		//Create anonymous inner class to define compareTo function.
-		QuickSort <Integer> q = new QuickSort<Integer>()
-		{
-			@Override
-			boolean compareTo(Integer a, Integer b) {return a>b;}
-		};
-        
-		//Create ArrayList
-        ArrayList <Integer> a = new ArrayList<>();
-        
-        //Size of array
-        int size = (int)Math.pow(2, 28);
-        
-		//Maximum value of elements
-		int maxVal = Integer.MAX_VALUE;
 
-		//Generate random integers.
-        for(int i = 0; i < size; i++) a.add((int)(maxVal*Math.random()));
-        
-		//Sort.
-        a = q.ascending(a);
-        
-        System.out.println("Sorted.");
+    /**
+     * Example for randomly generated integers of size 0 to Integer.MAX_VALUE.
+     *
+     * @param args
+     */
+    public static void main(String args[])
+    {
+	//Create anonymous inner class to define compareTo function.
+	QuickSort<Integer> q = new QuickSort<Integer>()
+	{
+	    @Override
+	    boolean compareTo(Integer a, Integer b) {return a > b;}
+	};
+
+	//Create ArrayList
+	ArrayList<Integer> a = new ArrayList<>();
+
+	//Size of array
+	int size = (int) Math.pow(2, 26);
+
+	//Maximum value of elements
+	int maxVal = Integer.MAX_VALUE;
+
+	//Generate random integers.
+	for (int i = 0; i < size; i++) a.add((int) (maxVal * Math.random()));
+
+	//Sort.
+	a = q.ascending(a);
+
+	System.out.println("Sorted.");
     }
 }
