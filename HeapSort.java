897b8a91c23887673ae81a418fcb00a076d69546
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2017-12-31 12:36:18 -0500

    Moved heap building mechanism to HeapSort.

--- HeapSort.java
+++ HeapSort.java
@@ -1,5 +1,3 @@
-package MadTools;
-
 import java.util.ArrayList;
 
 /**
@@ -7,126 +5,156 @@ import java.util.ArrayList;
  * @author Mad Scientist
  * @param <T>
  */
-class HeapSort <T>
+abstract class HeapSort <T>
 {
-	NodeComparator<T> nc;
+    NodeComparator<T> nc;
+	
+	HeapSort()
+	{
+	}
 	
 	HeapSort(NodeComparator<T> nc)
 	{
 		this.nc = nc;
 	}
-	
-	ArrayList<T> ascending(ArrayList<T> a)
-	{
-		Node <T> root = new Node<>(a.remove(0),nc);
-		a.forEach((x) -> {root.add(x);});
-		return root.getAscending();
-	}
-	
-	ArrayList<T> descending(ArrayList<T> a)
+    
+    abstract boolean compareTo(T a, T b);
+
+    ArrayList<T> ascending(ArrayList<T> a)
+    {
+        Node <T> root = new Node<>(a.remove(0));
+        Node <T> curr = root;
+
+        for(T x : a)
+        {
+            curr = root;
+
+            while(true)
+            {
+                if(compareTo(curr.val, x))
+                {
+                    if(curr.left==null)
+                    {
+                        curr.left = new Node<>(x);
+                        break;
+                    }
+                    else curr=curr.left;
+                }
+
+                else
+                {
+                    if(curr.right==null)
+                    {
+                        curr.right = new Node<>(x);
+                        break;
+                    }
+                    else curr=curr.right;
+                }
+            }
+        }
+
+        return root.getAscending();
+    }
+    
+    ArrayList<T> descending(ArrayList<T> a)
 	{
 		Node <T> root = new Node<>(a.remove(0),nc);
 		a.forEach((x) -> {root.add(x);});
 		return root.getDescending();
 	}
+
+    public static void main(String args[])
+    {
+        //Create anonymous inner class to define compareTo function.
+        HeapSort<Integer> h = new HeapSort<>()
+        {
+            @Override
+            boolean compareTo(Integer a, Integer b) {return a > b;}
+        };
+
+        //Test mode chooses small input size, max value, and prints input and output.
+        boolean test = true;
+
+        //Create ArrayList
+        ArrayList<Integer> a = new ArrayList<>();
+
+        //Size of array
+        int size = test ? 30 : (int)Math.pow(2, 26);
+
+        //Maximum value of elements
+        int maxVal = test ? 100 : Integer.MAX_VALUE;
+
+        //Generate random integers.
+        for (int i = 0; i < size; i++) a.add((int)(maxVal*Math.random()));
+
+        //Sort.
+        System.out.print(test ? a + "\n" : "");
+        a = h.ascending(a);
+        System.out.print(test ? a + "\n" : "");
+
+        //Check if result is sorted.
+        for(int i = 0; i < a.size()-1; i++)
+            if(h.compareTo(a.get(i),a.get(i+1)))
+            {
+                System.out.print("Not Sorted.\n");
+                return;
+            }
+
+        System.out.print("Sorted.\n");
+    }
+
+    static abstract class NodeComparator <T>
+    {
+	abstract boolean compareTo(T a, T b);
+    }
+    
+    class Node <T>
+    {
+        private final T val;
+
+        Node <T> left;
+        Node <T> right;
+
+        Node(T val)
+        {
+            this.val = val;
+            left = null;
+            right = null;
+        }
 	
-	static abstract class NodeComparator <T>
+	void add(T newVal)
 	{
-		abstract boolean compareTo(T a, T b);
-	}
-	
-	public static void main(String args[])
-    	{
-		//Create anonymous inner class to define compareTo function.
-		NodeComparator <Integer> nc = new NodeComparator<Integer>()
+		if(nc.compareTo(val, newVal))
 		{
-			@Override
-			boolean compareTo(Integer a, Integer b) {return a > b;}
-		};
-		
-		HeapSort<Integer> h = new HeapSort<>(nc);
-        
-        	//Test mode chooses small input size, max value, and prints input and output.
-        	boolean test = true;
-
-		//Create ArrayList
-		ArrayList<Integer> a = new ArrayList<>();
-
-		//Size of array
-		int size = test ? 30 : (int)Math.pow(2, 26);
-
-		//Maximum value of elements
-		int maxVal = test ? 100 : Integer.MAX_VALUE;
-
-		//Generate random integers.
-		for (int i = 0; i < size; i++) a.add((int)(maxVal*Math.random()));
-        
-        	//Sort.
-        	System.out.print(test ? a + "\n" : "");
-        	a = h.ascending(a);
-        	System.out.print(test ? a + "\n" : "");
-        
-        	//Check if result is sorted.
-        	for(int i = 0; i < a.size()-1; i++)
-            		if(nc.compareTo(a.get(i),a.get(i+1)))
-            		{
-                	System.out.print("Not Sorted.\n");
-                	return;
-            		}
-        
-		System.out.print("Sorted.\n");
-    	}
-	
-	class Node <T>
-	{
-		private final T val;
-		
-		Node <T> left;
-		Node <T> right;
-		
-		final NodeComparator <T> nc;
-		
-		Node(T val, NodeComparator <T> nc)
-		{
-			this.val = val;
-			left = null;
-			right = null;
-			this.nc = nc;
+			if(left==null) left = new Node<>(newVal,nc);
+			else left.add(newVal);
 		}
-		
-		void add(T newVal)
-		{
-			if(nc.compareTo(val, newVal))
-			{
-				if(left==null) left = new Node<>(newVal,nc);
-				else left.add(newVal);
-			}
 			
-			else
-			{
-				if(right==null) right = new Node<>(newVal,nc);
-				else right.add(newVal);
-			}
-		}
-		
-		ArrayList<T> getAscending()
+		else
 		{
-			ArrayList<T> result = new ArrayList<>();
-			if(left!=null) result.addAll(left.getAscending());
-			result.add(val);
-			if(right!=null) result.addAll(right.getAscending());
-			
-			return result;
-		}
-		
-		ArrayList<T> getDescending()
-		{
-			ArrayList<T> result = new ArrayList<>();
-			if(right!=null) result.addAll(right.getDescending());
-			result.add(val);
-			if(left!=null) result.addAll(left.getDescending());
-			
-			return result;
+			if(right==null) right = new Node<>(newVal,nc);
+			else right.add(newVal);
 		}
 	}
+
+        ArrayList<T> getAscending()
+        {
+            ArrayList<T> result = new ArrayList<>();
+            if(left!=null) result.addAll(left.getAscending());
+            result.add(val);
+            if(right!=null) result.addAll(right.getAscending());
+
+            return result;
+        }
+
+        ArrayList<T> getDescending()
+        {
+            ArrayList<T> result = new ArrayList<>();
+            if(right!=null) result.addAll(right.getDescending());
+            result.add(val);
+            if(left!=null) result.addAll(left.getDescending());
+
+            return result;
+        }
+    }
 }
