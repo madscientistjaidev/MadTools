8c4859f2fae17d36caf91fb260e048e5095776f5
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2018-01-06 03:34:14 -0500

    Added docs

--- HeapSort.java
+++ HeapSort.java
@@ -9,8 +9,12 @@ import java.util.ArrayList;
  */
 abstract class HeapSort <T>
 {
-	abstract boolean compareTo(T a, T b);
-
+	/**
+	 * Accepts an ArrayList and returns one sorted in ascending order.
+	 *
+	 * @param a
+	 * @return
+	 */
 	ArrayList<T> ascending(ArrayList<T> a)
 	{
 		Node <T> root = buildHeap(a);
@@ -18,6 +22,12 @@ abstract class HeapSort <T>
 		return root.getAscending();
 	}
 	
+	/**
+	 * Accepts an ArrayList and returns one sorted in descending order.
+	 *
+	 * @param a
+	 * @return
+	 */
 	ArrayList<T> descending(ArrayList<T> a)
 	{
 		Node <T> root = buildHeap(a);
@@ -25,6 +35,11 @@ abstract class HeapSort <T>
 		return root.getDescending();
 	}
 	
+	/**
+	 * Builds heap from ArrayList and returns root node.
+	 * @param a
+	 * @return 
+	 */
 	private Node <T> buildHeap(ArrayList<T> a)
 	{
 		Node <T> curr, root = new Node<>(a.remove(a.size()<3 ? 0 : getMedian(a.get(0),a.get(a.size()/2),a.get(a.size()-1),a.size())));
@@ -60,11 +75,35 @@ abstract class HeapSort <T>
 		return root;
 	}
 	
+	/**
+	 * Gets median of 3 elements to choose pivot.
+	 * Compares first, middle, and last elements, and returns index of median.
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param size
+	 * @return 
+	 */
 	private int getMedian(T a, T b, T c, int size)
 	{
 		return compareTo(a,b) ? (compareTo(c,a) ? 0 : (compareTo(c,b) ? size-1 : size/2)) : (compareTo(c,b) ? (size/2) : (compareTo(c,a) ? size-1 : 0));
 	}
+	
+	/**
+	 * Returns true if a>b. Should be implemented in a sub class or anonymous inner class.
+	 *
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	abstract boolean compareTo(T a, T b);
 
+	/**
+	 * Example for ~1 million randomly generated integers of size 0 to Integer.MAX_VALUE-1.
+	 *
+	 * @param args
+	 */
 	public static void main(String args[])
 	{
 		//Create anonymous inner class to define compareTo function.
@@ -81,7 +120,7 @@ abstract class HeapSort <T>
 		ArrayList<Integer> a = new ArrayList<>();
 
 		//Size of array
-		int size = test ? 30 : (int)Math.pow(2, 26);
+		int size = test ? 30 : (int)Math.pow(2, 20);
 
 		//Maximum value of elements
 		int maxVal = test ? 100 : Integer.MAX_VALUE;
