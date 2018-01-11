a63e7e21c432cee8ee23ff9d396464bb30bdc9eb
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2018-01-10 22:55:20 -0500

    Added docs

--- JumpSort.java
+++ JumpSort.java
@@ -8,7 +8,13 @@ import java.util.ArrayList;
  */
 abstract class JumpSort<T>
 {
-    ArrayList <T> ascending(ArrayList<T> a)
+    /**
+	 * Accepts an ArrayList and returns one sorted in ascending order.
+	 *
+	 * @param a
+	 * @return
+	 */
+	ArrayList <T> ascending(ArrayList<T> a)
     {
 		if(a==null) return new ArrayList<>();
 		if(a.size()<2) return a;
@@ -34,24 +40,6 @@ abstract class JumpSort<T>
 					for(int i = 0; i<a.size(); i++)
 						if(!compareTo(run.get(run.size()-1),a.get(i)))
 							run.add(a.remove(i--));
-				}
-
-				if(false && !runs.isEmpty())
-				{
-					for(ArrayList <T> z : runs)
-					{
-						if(compareTo(run.get(0),z.get(z.size()-1)))
-						{
-							z.addAll(run);
-							break;
-						}
-
-						else if(compareTo(z.get(0),run.get(run.size()-1)))
-						{
-							z.addAll(0, run);
-							break;
-						}
-					}
 				}
 
 				runs.add(run);
@@ -101,44 +89,56 @@ abstract class JumpSort<T>
 		return runs.get(0);
     }
 
+    /**
+	 * Returns true if a>b. Should be implemented in a sub class or anonymous inner class.
+	 *
+	 * @param a
+	 * @param b
+	 * @return
+	 */
     abstract boolean compareTo(T a, T b);
 
+    /**
+     * Example for ~1 million randomly generated integers of size 0 to Integer.MAX_VALUE.
+     *
+     * @param args
+     */
     public static void main(String args[])
     {
-	//Create anonymous inner class to define compareTo function.
-	JumpSort<Integer> j = new JumpSort<Integer>()
-	{
-	    @Override
-	    boolean compareTo(Integer a, Integer b) {return a > b;}
-	};
-        
-        boolean test = false;
+		//Create anonymous inner class to define compareTo function.
+		JumpSort<Integer> j = new JumpSort<Integer>()
+		{
+			@Override
+			boolean compareTo(Integer a, Integer b) {return a > b;}
+		};
 
-	//Create ArrayList
-	ArrayList<Integer> a = new ArrayList<>();
+			boolean test = false;
 
-	//Size of array
-	int size = test ? 30 : (int)Math.pow(2, 20);
+		//Create ArrayList
+		ArrayList<Integer> a = new ArrayList<>();
 
-	//Maximum value of elements
-	int maxVal = test ? 100 : Integer.MAX_VALUE;
+		//Size of array
+		int size = test ? 30 : (int)Math.pow(2, 20);
 
-	//Generate random integers.
-	for (int i = 0; i < size; i++)
-            a.add((int) (maxVal * Math.random()));
-        
-        //Sort.
-        System.out.print(test ? a + "\n" : "");
-        a = j.ascending(a);
-        System.out.print(test ? a + "\n" : "");
-        
-        for(int i = 0; i < a.size()-1; i++)
-            if(j.compareTo(a.get(i),a.get(i+1)))
-            {
-                System.out.print("Not Sorted.\n");
-                return;
-            }
-        
-	System.out.print("Sorted.\n");
+		//Maximum value of elements
+		int maxVal = test ? 100 : Integer.MAX_VALUE;
+
+		//Generate random integers.
+		for (int i = 0; i < size; i++)
+				a.add((int) (maxVal * Math.random()));
+
+			//Sort.
+			System.out.print(test ? a + "\n" : "");
+			a = j.ascending(a);
+			System.out.print(test ? a + "\n" : "");
+
+			for(int i = 0; i < a.size()-1; i++)
+				if(j.compareTo(a.get(i),a.get(i+1)))
+				{
+					System.out.print("Not Sorted.\n");
+					return;
+				}
+
+		System.out.print("Sorted.\n");
     }
 }
