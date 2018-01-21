904d59e89d36e581478e2c1ae3fc70c243294f79
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2018-01-21 12:29:11 -0500

    Reintroduced visited mechanism

--- Orienteering.java
+++ Orienteering.java
@@ -13,27 +13,28 @@ import java.util.Arrays;
  */
 public class Orienteering
 {
+	private static ArrayList<Integer[]> seen;
+	
 	/**
-	 * Stores list of visited locations.
+	 * Board.
 	 */
-	private static ArrayList<Integer[]> visited;
-	private char[][] grid;
+	private char[][] b;
 
 	/**
 	 * Accepts a board and returns the distance from source to destination if it exists, or -1 if it doesn't.
-	 * @param grid
+	 * @param b
 	 * @return
 	 */
-	public int findDist(char[][] grid)
+	public int findDist(char[][] b)
 	{
-		this.grid = grid;
+		this.b = b;
 		int dist;
 		
-		visited = new ArrayList<>();
+		seen = new ArrayList<>();
 
 		//Finding source
-		for(int i=0; i < grid.length; i++) for(int j=0; j<grid[0].length; j++)
-				if(grid[i][j]=='*') return ((dist=findDFS(i,j,0))>grid.length*grid[0].length) ? -1 : dist-1;
+		for(int i=0; i < b.length; i++) for(int j=0; j<b[0].length; j++)
+				if(b[i][j]=='*') return ((dist=findDFS(i,j,0))>b.length*b[0].length) ? -1 : dist-1;
 		
 		return -1;
 	}
@@ -44,42 +45,42 @@ public class Orienteering
 	 * @param grid
 	 * @param sourceX
 	 * @param sourceY
-	 * @param currX
-	 * @param currY
-	 * @param depth
+	 * @param x
+	 * @param y
+	 * @param d
 	 * @return 
 	 */
-	private int findDFS(int currX, int currY, int depth)
+	private int findDFS(int x, int y, int d)
 	{
-		addToVisited(currX,currY,depth);
+		visit(x,y,d);
 
 		//If destination found.
-		if(grid[currX][currY]=='#') return 1;
+		if(b[x][y]=='#') return 1;
 
 		//Theoretical maximum depth of recursion.
-		int max = grid.length*grid[0].length;
+		int max = b.length*b[0].length;
 
 		//If unpassable.
-		if(grid[currX][currY]=='X') return max+1;
+		if(b[x][y]=='X') return max+1;
 
 		//Check if max depth is reached.
-		if(depth>max) return max+1;
+		if(d>max) return max+1;
 
 		//Stores result of search in each direction. [l,r,u,d]
 		int[] res = new int[4];
 
 		//Depth First Search in each direction.
-		res[0] = (currX==0) || isVisited(currX-1, currY, depth+1) || (grid[currX-1][currY]=='X')
-					? max+1 : findDFS(currX-1, currY, depth+1);
+		res[0] = (x==0) || isSeen(x-1, y, d+1) || (b[x-1][y]=='X')
+					? max+1 : findDFS(x-1, y, d+1);
 
-		res[1] = (currX==grid.length-1) || isVisited(currX+1, currY, depth+1) || (grid[currX+1][currY]=='X')
-					? max+1 : findDFS(currX+1, currY, depth+1);
+		res[1] = (x==b.length-1) || isSeen(x+1, y, d+1) || (b[x+1][y]=='X')
+					? max+1 : findDFS(x+1, y, d+1);
 
-		res[2] = (currY==0) || isVisited(currX, currY-1, depth+1) || (grid[currX][currY-1]=='X')
-					? max+1 : findDFS(currX, currY-1, depth+1);
+		res[2] = (y==0) || isSeen(x, y-1, d+1) || (b[x][y-1]=='X')
+					? max+1 : findDFS(x, y-1, d+1);
 
-		res[3] = (currY==grid[0].length-1) || isVisited(currX, currY+1, depth+1) || (grid[currX][currY+1]=='X')
-					? max+1 : findDFS(currX, currY+1, depth+1);
+		res[3] = (y==b[0].length-1) || isSeen(x, y+1, d+1) || (b[x][y+1]=='X')
+					? max+1 : findDFS(x, y+1, d+1);
 
 		//Find minimum of four distances.
 		Arrays.sort(res);
@@ -88,35 +89,22 @@ public class Orienteering
 		return res[0]+1;
 	}
 	
-	/**
-	 * Returns true if point has been visited at a shallower or equal depth.
-	 * @param x
-	 * @param y
-	 * @param depth
-	 * @return 
-	 */
-	private boolean isVisited(int x, int y, int depth)
+	private boolean isSeen(int x, int y, int d)
 	{
-		return visited.stream().anyMatch((point) -> (point[0]==x && point[1]==y && point[2]<=depth));
+		return seen.stream().anyMatch((pt) -> (pt[0]==x && pt[1]==y && pt[2]<=d));
 	}
 	
-	/**
-	 * Adds point to visited if unseen, or updates depth if seen.
-	 * @param x
-	 * @param y
-	 * @param depth 
-	 */
-	void addToVisited(int x, int y, int depth)
+	void visit(int x, int y, int d)
 	{
-		for(Integer[] point : visited)
-			if(point[0]==x && point[1]==y)
+		for(Integer[] pt : seen)
+			if(pt[0]==x && pt[1]==y)
 			{
-				point[2]=depth;
+				pt[2]=d;
 				return;
 			}
 		
-		Integer[] newPoint = {x,y,depth};
-		visited.add(newPoint);
+		Integer[] newPt = {x,y,d};
+		seen.add(newPt);
 	}
 
 	/**
