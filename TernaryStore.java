c4f5477e6511ad5b8f9384f22d4bf477d3538962
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2018-01-29 00:39:23 -0500

    Added ascending

--- TernaryStore.java
+++ TernaryStore.java
@@ -11,20 +11,55 @@ public class TernaryStore<T>
 {
 	ArrayList<T> vals;
 	
+	/**
+	 * Used to build heap.
+	 * @param <T> 
+	 */
 	class TernaryNode <T>
 	{
+		/**
+		 * Value of current node.
+		 */
 		private final T val;
 
+		/**
+		 * Children of current node.
+		 */
 		TernaryNode<T> left;
 		TernaryNode<T> center;
 		TernaryNode <T> right;
 
+		/**
+		 * Initializes current node
+		 * @param val 
+		 */
 		TernaryNode(T val)
 		{
 			this.val = val;
 			left = null;
 			center = null;
 			right = null;
+		}
+
+		/**
+		 * Gets sub-tree elements in ascending order.
+		 * @return 
+		 */
+		ArrayList<T> getAscending()
+		{
+			ArrayList<T> result = new ArrayList<>();
+			if(left!=null) result.addAll(left.getAscending());
+			
+			result.add(val);
+			while(center!=null)
+			{
+				result.add(center.val);
+				center = center.center;
+			}
+			
+			if(right!=null) result.addAll(right.getAscending());
+			
+			return result;
 		}
 	}
 }
