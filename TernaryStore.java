cc98383c6a7eafeafe55c1c3b27460ae17ffd56f
Jaidev Ramakrishna <madscientistjaidev@gmail.com>
2018-02-09 22:24:33 -0500

    Modified data structures, added constructors.

--- TernaryStore.java
+++ TernaryStore.java
@@ -9,20 +9,32 @@ import java.util.ArrayList;
  */
 abstract class TernaryStore<T>
 {
-	ArrayList<T> vals;
+	TernaryStore(){}
+	
+	TernaryStore(T a) {root = new TernaryNode<>(a);}
+	
+	/**
+	 * Stores values in inserted order.
+	 */
+	ArrayList<TernaryStore<T>> vals;
+	
+	/**
+	 * Root node of Ternary Tree.
+	 */
+	TernaryNode<T> root;
 	
 	/**
 	 * Used to compare two values. Returns 1 if a>b, 0 if a=b, and -1 if a<b.
 	 * Should be implemented in a sub class or anonymous inner class.
 	 * @param a
 	 * @param b
-	 * @return 
+	 * @return
 	 */
 	abstract int compareTo(T a, T b);
 	
 	/**
-	 * Used to build heap.
-	 * @param <T> 
+	 * Used to build Ternary Tree.
+	 * @param <T>
 	 */
 	class TernaryNode <T>
 	{
@@ -34,13 +46,11 @@ abstract class TernaryStore<T>
 		/**
 		 * Children of current node.
 		 */
-		TernaryNode<T> left;
-		TernaryNode<T> center;
-		TernaryNode<T> right;
+		TernaryNode<T> left, center, right;
 
 		/**
 		 * Initializes current node
-		 * @param val 
+		 * @param val
 		 */
 		TernaryNode(T val)
 		{
@@ -52,7 +62,7 @@ abstract class TernaryStore<T>
 
 		/**
 		 * Gets sub-tree elements in ascending order.
-		 * @return 
+		 * @return
 		 */
 		ArrayList<T> getAscending()
 		{
@@ -73,7 +83,7 @@ abstract class TernaryStore<T>
 
 		/**
 		 * Gets sub-tree elements in descending order.
-		 * @return 
+		 * @return
 		 */
 		ArrayList<T> getDescending()
 		{
